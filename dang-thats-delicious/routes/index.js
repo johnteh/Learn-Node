2c3f373c08bba19036e63df7611ac462c32e98c7
John Ruane <john.ruane@jdwilliams.co.uk>
2017-06-19 09:03:59 +0100

    latest files

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const storeCotroller = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  res.render('hello', {
-	  name: 'wes',
-	  dog: req.query.dog
-  });
-});
+router.get('/', storeCotroller.homePage);
+router.get('/add', storeCotroller.addStore);
+router.post('/add', storeCotroller.createStore);
 
 module.exports = router;
