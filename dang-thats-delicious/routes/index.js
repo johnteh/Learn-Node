31e669f0f1ddb1469a3535424c2e5ab09eed04c0
John Ruane <john.ruane@jdwilliams.co.uk>
2017-07-03 16:29:40 +0100

    worked on files

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const storeCotroller = require('../controllers/storeController');
+const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
-router.get('/', storeCotroller.homePage);
-router.get('/add', storeCotroller.addStore);
-router.post('/add', storeCotroller.createStore);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+router.post('/add',
+	storeController.upload,
+	catchErrors(storeController.resize),
+	catchErrors(storeController.createStore)
+);
+router.post('/add/:id',
+	storeController.upload,
+	catchErrors(storeController.resize),
+	catchErrors(storeController.updateStore)
+);
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login/', userController.loginForm);
+router.get('/register/', userController.registerForm);
+router.post('/register/', userController.validateRegister);
 
 module.exports = router;
